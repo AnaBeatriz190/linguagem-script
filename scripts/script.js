5c15c090fb3b5148c1f86f9e4e208a863c86efe0
Ani <148493441+AnaBeatriz190@users.noreply.github.com>
2024-03-06 19:55:10 +0000

    salvamento 2.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,60 @@
-var nome = prompt('Qual o seu nome ?');
+/*var condicao = true; 
 
-document.getElementById('descrição').innerHTML += nome; 
+if(condicao){
+    var nome = prompt('Qual o seu nome ?');
+    document.getElementById('descrição').innerHTML += nome; 
+
+} else{
+    var idade = prompt('Qual a sua profissão ? ');
+}*/
+
+// var manha = true;
+// var tarde = false;
+// var noite = false;
+
+// if(manha){
+//     alert('Manhã');
+// }else if(tarde){
+//     alert('Tarde');
+// }else if(noite){
+//     alert('Noite')
+   
+// }
+
+// var nome = prompt("Qual o seu nome ?")
+
+// var resposta = confirm(nome + ", você gostaria de continuar ?")
+
+// if (resposta){
+//     alert('Bem-vindo...')
+// }else {
+//     window.location.href = '../index.html'
+// }
+
+// var preco = prompt('Qual o preço do produto')
+
+// if(preco <= 20){
+//     alert('Barato...')
+// }else{
+//     alert('Caro...')
+// }
+
+// var mensagem = (preco <20) ? 'Barato..' : 'Caro...';
+// alert(mensagem);
+
+
+// var mes = prompt("Digite um mês")
+// switch(mes){
+//     case '1':
+//         alert('Janeiro');
+//         break;
+//     case '2':
+//         alert('Fevereiro');
+//         break;
+//     case '3':
+//         alert('Março');
+//         break;
+//     default:
+//         alert('Mês inválido');
+//         break;
+// }
