952365a0bc702cc956c244a11b7f2b8e8c459f1b
Ani <148493441+AnaBeatriz190@users.noreply.github.com>
2024-03-27 17:20:05 +0000

    salvamento 6.0

---
+++ teste/teste.js
@@ -0,0 +1,48 @@
+
+/*confirm("Hello World");
+prompt("Hello World!");
+alert("Hello World !"); 
+console.log("Hello World:"); 
+
+
+
+var carro = "HB20";
+var nome; 
+
+console.log(pi)
+console.log(ome)
+console.log(Luciano)
+
+var pi = 3.14;
+var nome = "Luciano";
+var resposta = "Sim";
+
+var disciplina = "Linguagens de Script,", professor = "Luciano", matricula = "00000";
+
+document.getElementById("disciplina").innerHTML = disciplina;
+
+var nome;
+console.log(typeof nome);
+
+nome = "Luciano";
+console.log(typeof nome);
+
+nome = 10;
+console.log(typeof nome);
+
+var nome = "lucianon" + "alexandre"
+
+console.log(nome)
+
+var alunos = ["Ana", "Maria", "João", "Josefa"]
+console.log(alunos[2])
+
+var num1 = 10
+var num2 = 50
+var soma = num1 + num2
+
+alert(soma)*/
+
+var km = 0;
+km =+ 10;
+alert(soma)
